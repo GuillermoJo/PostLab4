5a07c79ee4ca373a638021daaf2049755f70ec48
GuillermoJo <jo24616@uvg.edu.gt>
2026-04-13 23:51:22 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * PostLab4.c
+ *
+ * Created: 13/04/2026 23:40:25
+ * Author : Guillermo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
